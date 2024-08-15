698ef5a0d4be30504cf8f9630a272feafbb2e8f5
ferharo33 <166965921+ferharo33@users.noreply.github.com>
2024-08-14 23:00:44 -0600

    Update repayment_lines.py

--- advanced_loan_management/models/repayment_lines.py
+++ advanced_loan_management/models/repayment_lines.py
@@ -123,8 +123,8 @@ class RepaymentLine(models.Model):
         })
         if invoice:
             invoice.action_post()
-            self.invoice=True
-            self.write({'state':'invoiced'})
+            rec.invoice = True
+            rec.state = 'invoiced'
         return {
             'name': 'Invoice',
             'res_model': 'account.move',

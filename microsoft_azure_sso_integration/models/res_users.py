30cc68d16529a9e6a6bf927e64a9af75eb66d49b
ferharo33 <166965921+ferharo33@users.noreply.github.com>
2024-09-25 15:43:44 -0600

    Add files via upload

--- microsoft_azure_sso_integration/models/res_users.py
+++ microsoft_azure_sso_integration/models/res_users.py
@@ -4,6 +4,7 @@ import requests
 
 from odoo import api, models, _
 from odoo.http import request
+from odoo.addons import base
 from odoo.addons.auth_signup.models.res_users import SignupError
 
 _logger = logging.getLogger(__name__)
@@ -20,64 +21,77 @@ class ResUsers(models.Model):
     @api.model
     def _auth_oauth_rpc(self, endpoint, access_token):
         """ Pass the response of sign in """
-        _logger.info(f"Calling OAuth endpoint: {endpoint} with access token.")
         res = super()._auth_oauth_rpc(endpoint, access_token)
-        _logger.info(f"OAuth endpoint response: {res}")
         if endpoint:
             return requests.get(endpoint, params={'access_token': access_token}).json()
         return res
-    
+
     @api.model
     def _auth_oauth_code_validate(self, provider, code):
-        _logger.info(f"Validating OAuth code for provider: {provider} with code: {code}")
+        """ Return the validation data corresponding to the access token """
         auth_oauth_provider = self.env['auth.oauth.provider'].browse(provider)
-        validation_data = super(ResUsers, self)._auth_oauth_code_validate(provider, code)
-        
-        _logger.info(f"Validation data received: {validation_data}")
+        # AJUSTES FH
+        redirect_uri = 'https://' + request.httprequest.host + '/auth_oauth/signin'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        }
+        token_info = requests.post(
+            auth_oauth_provider.validation_endpoint,
+            headers=headers,
+            data={
+                'client_id': auth_oauth_provider.client_id,
+                'client_secret': auth_oauth_provider.client_secret_id,
+                'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': redirect_uri
+            }
+        ).json()
 
-        # Search for the user based on the OAuth UID
-        user_oauth = self.search([('oauth_uid', '=', validation_data.get('user_id'))])
-        _logger.info(f"User search result for OAuth UID: {validation_data.get('user_id')}, found: {user_oauth}")
-        
-        if not user_oauth:
-            # If user is not found, raise an error instead of creating a new user
-            _logger.warning(f"Access Denied: No user found with OAuth UID: {validation_data.get('user_id')}")
-            raise AccessDenied(_("Access Denied: User is not registered in Odoo. Please contact your administrator."))
-        
-        _logger.info(f"User {user_oauth} found, proceeding with authentication.")
-        return validation_data
+        if token_info.get("error"):
+            raise Exception(token_info['error'])
+        access_token = token_info.get('access_token')
+        validation = {
+            'access_token': access_token
+        }
+        if token_info.get('id_token'):
+            if not jwt:
+                raise Exception("Access Denied")
+            #data = jwt.decode(token_info['id_token'], verify=False)
+            data = jwt.decode(token_info['id_token'], options={"verify_signature": False}, algorithms=["RS256"])
+        else:
+            data = self._auth_oauth_rpc(auth_oauth_provider.data_endpoint, access_token)
+        validation.update(data)
+        return validation
 
     @api.model
     def _auth_oauth_signin(self, provider, validation, params):
-        _logger.info(f"Signing in user with provider: {provider} and validation data: {validation}")
+        """ Retrieve and sign in the user corresponding to provider and validated access token """
         user = self.search([('login', '=', str(validation.get('email')))])
-        
         if not user:
-            _logger.info(f"No user found with email {validation.get('email')}, creating new user.")
             user = self.create({
                 'login': str(validation.get('email')),
                 'name': str(validation.get('name'))
             })
-        
-        _logger.info(f"Updating user OAuth info for user: {user}")
+            provider_id = self.env['auth.oauth.provider'].sudo().browse(provider)
+            if provider_id.template_user_id:
+                user.is_contractor = provider_id.template_user_id.is_contractor
+                user.contractor = provider_id.template_user_id.contractor
+                user.groups_id = [(6, 0, provider_id.template_user_id.groups_id.ids)]
         user.write({
             'oauth_provider_id': provider,
             'oauth_uid': validation['user_id'],
             'oauth_access_token': params['access_token'],
         })
-
+        oauth_uid = validation['user_id']
         try:
-            oauth_uid = validation['user_id']
             oauth_user = self.search([("oauth_uid", "=", oauth_uid), ('oauth_provider_id', '=', provider)])
             if not oauth_user:
-                _logger.warning(f"Access Denied: No OAuth user found with UID: {oauth_uid}")
                 raise Exception("Access Denied")
             assert len(oauth_user) == 1
             oauth_user.write({'oauth_access_token': params['access_token']})
-            _logger.info(f"User {oauth_user.login} successfully signed in with OAuth.")
             return oauth_user.login
-        except (Exception, Exception) as e:
-            _logger.error(f"Error during OAuth sign-in: {e}")
+        except (Exception, Exception):
             if self.env.context.get('no_user_creation'):
                 return None
             state = json.loads(params['state'])
@@ -86,14 +100,13 @@ class ResUsers(models.Model):
             try:
                 _, login, _ = self.signup(values, token)
                 return login
-            except SignupError as e:
-                _logger.error(f"Signup error: {e}")
+            except SignupError:
                 raise Exception("Access Denied")
         return super(ResUsers, self)._auth_oauth_signin(provider, validation, params)
 
     @api.model
     def auth_oauth(self, provider, params):
-        _logger.info(f"Starting OAuth authentication for provider: {provider} with params: {params}")
+        """ Take the access token to log in with the user account """
         if params.get('code'):
             validation = self._auth_oauth_code_validate(provider, params['code'])
             access_token = validation.pop('access_token')
@@ -101,24 +114,16 @@ class ResUsers(models.Model):
         else:
             access_token = params.get('access_token')
             validation = self._auth_oauth_validate(provider, access_token)
-        
-        _logger.info(f"Validation result: {validation}")
-
         if not validation.get('user_id'):
-            _logger.warning(f"Validation failed: No user_id in validation data: {validation}")
             if validation.get('id'):
                 validation['user_id'] = validation['id']
             elif validation.get('oid'):
                 validation['user_id'] = validation['oid']
             else:
                 raise Exception("Access Denied")
-        
         login = self._auth_oauth_signin(provider, validation, params)
         if not login:
-            _logger.warning(f"Access Denied: Unable to sign in user with provider {provider}.")
             raise Exception("Access Denied")
-        
-        _logger.info(f"OAuth authentication successful for user: {login}")
         if provider and params:
             return self.env.cr.dbname, login, access_token
         return super(ResUsers, self).auth_oauth(provider, params)
